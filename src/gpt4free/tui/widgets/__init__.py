129145632786329ed0ccaf34176b2a92e24de530
12karat <vitaliyspirin0@gmail.com>
2026-06-28 19:37:19 +0000

    tui(widgets): export StatusScreen, finalize widgets package __all__

--- src/gpt4free/tui/widgets/__init__.py
+++ src/gpt4free/tui/widgets/__init__.py
@@ -2,9 +2,11 @@
 
 from gpt4free.tui.widgets.chat_log import ChatLog
 from gpt4free.tui.widgets.pickers import ModelPickerScreen, ProviderPickerScreen
+from gpt4free.tui.widgets.status import StatusScreen
 
 __all__ = [
     "ChatLog",
     "ModelPickerScreen",
     "ProviderPickerScreen",
+    "StatusScreen",
 ]
