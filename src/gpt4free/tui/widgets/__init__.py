0f273bcb9c0a1ad92f63c8c1fb15db1512bbc26d
12karat <vitaliyspirin0@gmail.com>
2026-06-28 19:35:37 +0000

    tui(widgets): export ModelPickerScreen and ProviderPickerScreen

--- src/gpt4free/tui/widgets/__init__.py
+++ src/gpt4free/tui/widgets/__init__.py
@@ -1,7 +1,10 @@
 """TUI widget package."""
 
 from gpt4free.tui.widgets.chat_log import ChatLog
+from gpt4free.tui.widgets.pickers import ModelPickerScreen, ProviderPickerScreen
 
 __all__ = [
     "ChatLog",
+    "ModelPickerScreen",
+    "ProviderPickerScreen",
 ]
