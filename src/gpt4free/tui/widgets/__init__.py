1e3ccfa1d2c72237907f649082a396cb5e3141e0
Vitaliy Spirin <vitaliyspirin0@gmail.com>
2026-07-02 11:39:22 +0800

    tui(widgets): sort and finalize __all__ export list

--- src/gpt4free/tui/widgets/__init__.py
+++ src/gpt4free/tui/widgets/__init__.py
@@ -1,17 +1,17 @@
 """TUI widget package."""
 
 from gpt4free.tui.widgets.chat_log import ChatLog
-from gpt4free.tui.widgets.pickers import ModelPickerScreen, ProviderPickerScreen
 from gpt4free.tui.widgets.keys_screen import CustomProviderScreen, KeysScreen
+from gpt4free.tui.widgets.pickers import ModelPickerScreen, ProviderPickerScreen
 from gpt4free.tui.widgets.proxy import ProxyScreen
 from gpt4free.tui.widgets.status import StatusScreen
 
 __all__ = [
     "ChatLog",
+    "CustomProviderScreen",
+    "KeysScreen",
     "ModelPickerScreen",
     "ProviderPickerScreen",
-    "KeysScreen",
-    "CustomProviderScreen",
     "ProxyScreen",
     "StatusScreen",
 ]
